fb79267fbc31a9b413e21cbe18a9901b6e9f3a14
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-12-16 01:47:14 +0100

    Register user controller

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5500;
 
+//db connection
+const dbConnection = require("./db/dbConfig");
+
 // Import routes
 const userRoutes = require("./routes/userRoute");
-const questionRoutes = require("./routes/questionRoute");
-const answerRoutes = require("./routes/answerRoute");
+// const questionRoutes = require("./routes/questionRoute");
+// const answerRoutes = require("./routes/answerRoute");
+
+//json middleware to extract Json data
+app.use(express.json());
 
 // User routes middleware
 app.use("/api/user", userRoutes);
@@ -16,11 +22,16 @@ app.use("/api/user", userRoutes);
 // Answer routes middleware
 // app.use("/api/answer", answerRoutes);
 
-// Start the server
-app.listen(port, (err) => {
-  if (err) {
-    console.log("Error:", err.message);
-  } else {
-    console.log(`Server is listening on port ${port}`);
+async function Start() {
+  try {
+    const result = await dbConnection.execute("select 'test' ")
+    await app.listen(port)
+    console.log("database connection established successfully!  ")
+    console.log(`Server running on port ${port}`)
+  } catch (error) {
+    console.log(error.message);
   }
-});
+}
+Start();
+
+
--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,54 @@
-function register(req, res) {
-  res.send("register");
+const e = require("express");
+const dbConnection = require("../db/dbConfig");
+const bcrypt = require("bcrypt");
+const { statusCodes } = require("http-status-codes");
+
+async function register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
+  if (!email || !password || !firstname || !lastname || !username) {
+    return res.status(400).json({ msg: "please provide all required fields" });
+  }
+  try {
+    const [user] = await dbConnection.query(
+      "SELECT username, userid FROM users WHERE username =? or email =?",
+      [username, email]
+    );
+    if (user.length > 0) {
+      return res
+        .status(statusCodes.BAD_REQUEST)
+        .json({ msg: "username already exists" });
+    }
+    if (password.length <= 8) {
+      return res
+        .status(statusCodes.BAD_REQUEST)
+        .json({ msg: "Password should be at least 8 characters long" });
+    }
+    //encrypt the password
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await dbConnection.query(
+      "INSERT INTO users (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)",
+      [username, firstname, lastname, email, hashedPassword]
+    );
+
+    return res
+      .status(statusCodes.CREATED)
+      .json({ msg: "User registed successfully!" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "something went wrong, try again later!" });
+  }
 }
 
-function login(req, res) {
+async function login(req, res) {
   res.send("login");
 }
 
-function checkUser(req, res) {
+async function checkUser(req, res) {
   res.send("check user");
 }
 
